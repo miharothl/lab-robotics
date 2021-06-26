b65c63fb4d09e20538e85d4accba928e2e1b8416
Miha <miha@rothl.com>
2021-06-26 15:10:46 +0100

    prepare to submit

--- project-05-home-service-robot/scripts/home_service.sh
+++ project-05-home-service-robot/scripts/home_service.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch;" &
+xterm  -e  "export ROBOT_INITIAL_POSE='-x -2.9 -y -2.31 -z 0 -R 0 -P 0 -Y 0';
+            roslaunch turtlebot_gazebo turtlebot_world.launch;" &
 sleep 5
  
 xterm  -e  "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../src/map/map.yaml;" &
