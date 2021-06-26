feea99dbc8f9daed5eb7a27b6d1a7144c672a526
root <root@ip-172-31-5-254.eu-west-1.compute.internal>
2021-06-26 14:46:50 +0000

    prepare to submit

--- project-05-home-service-robot/scripts/home_service.sh
+++ project-05-home-service-robot/scripts/home_service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xterm  -e  "export ROBOT_INITIAL_POSE='-x -2.9 -y -2.31 -z 0 -R 0 -P 0 -Y 0';
+xterm  -e  "export ROBOT_INITIAL_POSE='-x 0 -y 0 -z 0 -R 0 -P 0 -Y 0';
             roslaunch turtlebot_gazebo turtlebot_world.launch;" &
 sleep 5
  
